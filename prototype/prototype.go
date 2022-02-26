beeacea30019df15957038d4a04789d9cd5fa44a
stonebird <1245863260@qq.com>
2022-02-26 14:54:37 +0800

    原型模式

---
+++ prototype/prototype.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: prototype.go
+// @Date: 2022/2/26 14:22
+// @Desc: 原型模式：原型模式能够负责自身对象
+package prototype
+
+// Cloneable 是原型对象实现的接口
+type Cloneable interface {
+	Clone() Cloneable
+}
+
+type PrototypeManager struct {
+	prototypes map[string]Cloneable
+}
+
+func NewPrototypeManager() *PrototypeManager {
+	return &PrototypeManager{
+		prototypes: make(map[string]Cloneable),
+	}
+}
+
+func (p *PrototypeManager) Get(name string) Cloneable {
+	return p.prototypes[name].Clone()
+}
+
+func (p *PrototypeManager) Set(name string, prototype Cloneable) {
+	p.prototypes[name] = prototype
+}
---
+++ prototype/prototype_test.go
@@ -0,0 +1,58 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: prototype_test.go
+// @Date: 2022/2/26 14:30
+// @Desc: 原型模式测试方法
+package prototype
+
+import "testing"
+
+var manager *PrototypeManager
+
+type Type1 struct {
+	name string
+}
+
+func (t *Type1) Clone() Cloneable {
+	tc := *t
+	return &tc
+}
+
+type Type2 struct {
+	name string
+}
+
+func (t *Type2) Clone() Cloneable {
+	tc := *t
+	return &tc
+}
+
+func TestClone(t *testing.T) {
+	t1 := manager.Get("t1")
+
+	t2 := t1.Clone()
+
+	if t1 == t2 {
+		t.Fatal("error! get clone not working")
+	}
+}
+
+func TestCloneFromManager(t *testing.T) {
+	c := manager.Get("t1").Clone()
+
+	t1 := c.(*Type1)
+	if t1.name != "type1" {
+		t.Fatal("error")
+	}
+
+}
+
+func init() {
+	manager = NewPrototypeManager()
+
+	t1 := &Type1{
+		name: "type1",
+	}
+	manager.Set("t1", t1)
+}
