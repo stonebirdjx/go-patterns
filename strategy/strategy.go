e453c29aba1eeacfee0770535b45e3310ae30763
stonebird <1245863260@qq.com>
2022-02-28 21:19:51 +0800

    策略模式

---
+++ strategy/strategy.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: strategy.go
+// @Date: 2022/2/28 21:00
+// @Desc: 策略模式：可以动态地让一个对象在许多行为中选择一种行为
+package strategy
+
+import "fmt"
+
+type Payment struct {
+	context  *PaymentContext
+	strategy PaymentStrategy
+}
+
+type PaymentContext struct {
+	Name, CardID string
+	Money        int
+}
+
+func NewPayment(name, cardid string, money int, strategy PaymentStrategy) *Payment {
+	return &Payment{
+		context: &PaymentContext{
+			Name:   name,
+			CardID: cardid,
+			Money:  money,
+		},
+		strategy: strategy,
+	}
+}
+
+func (p *Payment) Pay() {
+	p.strategy.Pay(p.context)
+}
+
+type PaymentStrategy interface {
+	Pay(*PaymentContext)
+}
+
+type Cash struct{}
+
+func (*Cash) Pay(ctx *PaymentContext) {
+	fmt.Printf("Pay $%d to %s by cash", ctx.Money, ctx.Name)
+}
+
+type Bank struct{}
+
+func (*Bank) Pay(ctx *PaymentContext) {
+	fmt.Printf("Pay $%d to %s by bank account %s", ctx.Money, ctx.Name, ctx.CardID)
+
+}
---
+++ strategy/strategy_test.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: strategy_test.go
+// @Date: 2022/2/28 21:00
+// @Desc: 策略模式测试方法
+package strategy
+
+func ExamplePayByCash() {
+	payment := NewPayment("Ada", "", 123, &Cash{})
+	payment.Pay()
+	// Output:
+	// Pay $123 to Ada by cash
+}
+
+func ExamplePayByBank() {
+	payment := NewPayment("Bob", "0002", 888, &Bank{})
+	payment.Pay()
+	// Output:
+	// Pay $888 to Bob by bank account 0002
+}
