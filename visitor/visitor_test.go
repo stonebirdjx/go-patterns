58b0f51d34f734dbbfa682e8c8469d900c6bbcb5
stonebird <1245863260@qq.com>
2022-03-02 09:30:13 +0800

    访问者模式

---
+++ visitor/visitor_test.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: visitor_test.go
+// @Date: 2022/3/2 8:55
+// @Desc: 访问者模式的测试方法
+package visitor
+
+func ExampleRequestVisitor() {
+	c := &CustomerCol{}
+	c.Add(NewEnterpriseCustomer("A company"))
+	c.Add(NewEnterpriseCustomer("B company"))
+	c.Add(NewIndividualCustomer("bob"))
+	c.Accept(&ServiceRequestVisitor{})
+	// Output:
+	// serving enterprise customer A company
+	// serving enterprise customer B company
+	// serving individual customer bob
+}
+
+func ExampleAnalysis() {
+	c := &CustomerCol{}
+	c.Add(NewEnterpriseCustomer("A company"))
+	c.Add(NewIndividualCustomer("bob"))
+	c.Add(NewEnterpriseCustomer("B company"))
+	c.Accept(&AnalysisVisitor{})
+	// Output:
+	// analysis enterprise customer A company
+	// analysis enterprise customer B company
+}
