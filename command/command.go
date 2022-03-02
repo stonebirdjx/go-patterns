019a6b877222e3206cbbe8540e5bb72b7158b4f5
stonebird <1245863260@qq.com>
2022-03-02 12:05:05 +0800

    命令模式

---
+++ command/command.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: command.go
+// @Date: 2022/3/2 11:44
+// @Desc: 命令模式：把某个对象的方法调用封装到对象中，方便传递、存储、调用
+package command
+
+import "fmt"
+
+type Command interface {
+	Execute()
+}
+
+type StartCommand struct {
+	mb *MotherBoard
+}
+
+func NewStartCommand(mb *MotherBoard) *StartCommand {
+	return &StartCommand{
+		mb: mb,
+	}
+}
+
+func (c *StartCommand) Execute() {
+	c.mb.Start()
+}
+
+type RebootCommand struct {
+	mb *MotherBoard
+}
+
+func NewRebootCommand(mb *MotherBoard) *RebootCommand {
+	return &RebootCommand{
+		mb: mb,
+	}
+}
+
+func (c *RebootCommand) Execute() {
+	c.mb.Reboot()
+}
+
+type MotherBoard struct{}
+
+func (*MotherBoard) Start() {
+	fmt.Print("system starting\n")
+}
+
+func (*MotherBoard) Reboot() {
+	fmt.Print("system rebooting\n")
+}
+
+type Box struct {
+	button1 Command
+	button2 Command
+}
+
+func NewBox(button1, button2 Command) *Box {
+	return &Box{
+		button1: button1,
+		button2: button2,
+	}
+}
+
+func (b *Box) PressButton1() {
+	b.button1.Execute()
+}
+
+func (b *Box) PressButton2() {
+	b.button2.Execute()
+}
---
+++ command/command_test.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 hu. All rights reserved.
+// @Author: stonebirdjx
+// @Email: 1245863260@qq.com, g1245863260@gmail.com
+// @File: command_test.go
+// @Date: 2022/3/2 11:44
+// @Desc: 命令模式测试方法
+package command
+
+func ExampleCommand() {
+	mb := &MotherBoard{}
+	startCommand := NewStartCommand(mb)
+	rebootCommand := NewRebootCommand(mb)
+
+	box1 := NewBox(startCommand, rebootCommand)
+	box1.PressButton1()
+	box1.PressButton2()
+
+	box2 := NewBox(rebootCommand, startCommand)
+	box2.PressButton1()
+	box2.PressButton2()
+	// Output:
+	// system starting
+	// system rebooting
+	// system rebooting
+	// system starting
+}
